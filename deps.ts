3636eddcad961237eb8cabe3fb15e09b0e610367
bcajales <bernardocajales@gmail.com>
2025-09-14 18:27:00 -0300

    Delete deps.ts

--- deps.ts
+++
@@ -1,4 +0,0 @@
-// Archivo: deps.ts
-export { serve } from "https://deno.land/std@0.224.0/http/server.ts";
-export { chromium } from "https://deno.land/x/playwright@v1.44.0/mod.ts"; // <-- CAMBIO PRINCIPAL
-export * as cheerio from "https://esm.sh/cheerio@1.0.0-rc.12";
